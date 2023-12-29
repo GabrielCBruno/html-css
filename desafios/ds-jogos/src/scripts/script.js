08b179d98384385ac7d8a1082ec1c8326b832a4c
GabrielCBruno <142371512+GabrielCBruno@users.noreply.github.com>
2023-12-29 19:53:20 -0300

    Adição desafio jogos (versão Mobile First)

---
+++ desafios/ds-jogos/src/scripts/script.js
@@ -0,0 +1,15 @@
+const menu = document.getElementById('itens')
+
+function interacao() {
+    if (menu.style.display == 'none') {
+        menu.style.display = 'block'
+    }else {
+        menu.style.display = 'none'
+    }
+}
+
+function verificar() {
+    if (menu.style.display == 'none') {
+        menu.style.display = 'block'
+    }
+}
