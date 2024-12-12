22b125615831e31b302d32c2387a4fe93c200638
Gabriel <142371512+GabrielCBruno@users.noreply.github.com>
2024-12-12 00:36:45 -0300

    add

---
+++ desafios/Blog-Mundo-Anime/src/scritps/script.js
@@ -0,0 +1,10 @@
+var burger = document.getElementById('burger')
+var navegacao = document.getElementById('navegacao')
+
+function Menu() {
+    if (navegacao.style.display == 'none') {
+        navegacao.style.display == 'block'
+    }else {
+        navegacao.style.display == 'none'
+    }
+}
