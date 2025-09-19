b13e3828839bd86a40ab167280a2c55748b0c2ae
Gabriel <142371512+GabrielCBruno@users.noreply.github.com>
2025-09-19 20:20:26 -0300

    Correção

--- desafios/JogoDaAdinhação/src/codigos/script.js
+++ desafios/JogoDaAdinhação/src/codigos/script.js
@@ -1,7 +1,12 @@
 const divs = document.querySelectorAll(".itens");
 const botao = document.getElementById("meuBotao");
+const pontos = document.getElementById("saidaPontos");
+const meta = document.getElementById("saidaMeta");
 
 let intervaloId = null;
+
+let pontosI;
+let metaI=0;
 
 botao.addEventListener('click', () => {
   if (botao.innerText === "Clique aqui Para Iniciar") {
@@ -9,23 +14,19 @@ botao.addEventListener('click', () => {
       div.style.pointerEvents = "auto";
     });
 
+    pontosI = 0;
+
     botao.innerText = "Resetar";
 
     // Começa a escolher divs aleatórias a cada 1 segundo
     escolherDivAleatoria(); // Escolhe uma imediatamente
-    intervaloId = setInterval(escolherDivAleatoria, 1000); // depois a cada 1 segundo
+    intervaloId = setInterval(escolherDivAleatoria, 800); // depois a cada 1 segundo
 
   } else {
-    botao.innerText = "Clique aqui Para Iniciar";
-
-    divs.forEach(div => {
-      div.style.pointerEvents = "none";
-      div.removeAttribute('id'); // remove "correto" de todas
-    });
-
-    // Para a troca automática
-    clearInterval(intervaloId);
-    intervaloId = null;
+	botao.innerText = "Clique aqui Para Iniciar";
+    
+	/* Para a troca automática */
+    	encerrarPrograma();
   }
 });
 
@@ -41,9 +42,32 @@ function escolherDivAleatoria() {
 divs.forEach(div => {
   div.addEventListener('click', () => {
     if (div.id === 'correto') {
-      alert('Você clicou na div correta!');
+      
+      pontosI++;
+      pontos.innerText = pontosI;
     } else {
       alert('Você clicou na div errada!');
+      encerrarPrograma();
     }
   });
 });
+
+function encerrarPrograma() {
+
+    botao.innerText = "Clique aqui Para Iniciar";
+    
+    divs.forEach(div => {
+      div.style.pointerEvents = "none";
+      div.removeAttribute('id'); // remove "correto" de todas
+    });
+
+    clearInterval(intervaloId);
+    intervaloId = null;
+
+    if (pontosI > metaI) {
+	metaI = pontosI;
+     }
+
+    pontos.innerText = pontosI;
+    meta.innerText = metaI;   
+}
