82e101fac730427bc55f30d41c3082087a027872
Gabriel <142371512+GabrielCBruno@users.noreply.github.com>
2025-09-23 21:23:48 -0300

    Adição desafio Jogo da Adivinhação

--- desafios/Jogo da Adivinhação/src/codigos/script.js
+++ desafios/Jogo da Adivinhação/src/codigos/script.js
@@ -1,12 +1,31 @@
 const divs = document.querySelectorAll(".itens");
-const botao = document.getElementById("meuBotao");
+const botao = document.getElementById("btnIniciar");
 const pontos = document.getElementById("saidaPontos");
 const meta = document.getElementById("saidaMeta");
+const bntDificuldade = document.querySelectorAll(".btndificuldade");
 
 let intervaloId = null;
-
 let pontosI;
 let metaI=0;
+let velocidade = 500;
+
+bntDificuldade.forEach((btn, index) => {
+	btn.addEventListener('click', () => {
+	   
+
+	   if (index === 0) {
+		velocidade = 1000;
+	   } else if (index === 1) {
+		velocidade = 500;
+	   } else if (index === 2) {
+		velocidade = 300;
+           }
+
+	   btn.classList.add('btndificuldade-pressionado');
+
+	   bntDificuldade.forEach(b => b.style.pointerEvents = 'none'); 
+   });
+});
 
 botao.addEventListener('click', () => {
   if (botao.innerText === "Clique aqui Para Iniciar") {
@@ -20,11 +39,9 @@ botao.addEventListener('click', () => {
 
     // Começa a escolher divs aleatórias a cada 1 segundo
     escolherDivAleatoria(); // Escolhe uma imediatamente
-    intervaloId = setInterval(escolherDivAleatoria, 800); // depois a cada 1 segundo
+    intervaloId = setInterval(escolherDivAleatoria, velocidade); // depois a cada 1 segundo
 
   } else {
-	botao.innerText = "Clique aqui Para Iniciar";
-    
 	/* Para a troca automática */
     	encerrarPrograma();
   }
@@ -46,15 +63,16 @@ divs.forEach(div => {
       pontosI++;
       pontos.innerText = pontosI;
     } else {
-      alert('Você clicou na div errada!');
+      alert('Você clicou no quadrado errado!');
       encerrarPrograma();
     }
   });
 });
 
 function encerrarPrograma() {
-
     botao.innerText = "Clique aqui Para Iniciar";
+    bntDificuldade.forEach(b => b.style.pointerEvents = 'auto');
+    bntDificuldade.forEach(b => b.classList.remove('btndificuldade-pressionado')); 
     
     divs.forEach(div => {
       div.style.pointerEvents = "none";
