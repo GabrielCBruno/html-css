46e088c066c9dc8fe0f86c599e23c10df54548bc
Gabriel <142371512+GabrielCBruno@users.noreply.github.com>
2026-06-17 20:00:56 -0300

    Deletado blog mundo anime

--- desafios/Blog-Mundo-Anime/src/scritps/script.js
+++
@@ -1,9 +0,0 @@
-const menu = document.getElementById('navegacao')
-
-function Interacao() {
-    if (menu.style.display == 'none') {
-        menu.style.display = 'block'
-    }else {
-        menu.style.display = 'none'
-    }
-}
